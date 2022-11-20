586c8840034f77f6c53a72a0bad74ea47083fe08
Yuyan Li <yuyanli1211@gmail.com>
2022-11-20 16:53:43 +1100

    fixes

--- dataset.py
+++ dataset.py
@@ -1,13 +1,15 @@
 import json
 import os
+import random
 
 import numpy as np
 from PIL import Image
 from torch.utils.data import Dataset
-from torchvision import transforms
+from torchvision.io import read_image
+from torchvision.transforms import transforms
 
 
-class VideoData(Dataset):
+class TempVideoData(Dataset):
     def __init__(self, mode='train', image_set='2'):
         self.root = f'breakup_set/train' if mode == 'train' else f'breakup_set/test'
         self.length = 0
@@ -18,7 +20,6 @@ class VideoData(Dataset):
 
     def __getitem__(self, item):
         img, gt = self.dataset[item]
-        # print('got item')
         return img, gt
 
     def load_data(self):
@@ -27,7 +28,6 @@ class VideoData(Dataset):
             image_files = [f for f in files if f.endswith('.jpg')]
             for file in image_files:
                 img_path = os.path.join(root, file)
-                # print(img_path)
                 gt_path = img_path.replace('.jpg', '.json')
                 if not os.path.exists(img_path) or not os.path.exists(gt_path):
                     raise IOError("{} does not exist".format(img_path))
@@ -36,6 +36,32 @@ class VideoData(Dataset):
                 img_tensor = trans(img)
                 with open(gt_path) as gt_file:
                     gt = np.asarray(json.load(gt_file))
-                dataset.append([img_tensor, gt])
+                dataset.append([img, gt])
                 self.length += 1
         return dataset
+
+
+class VideoData(Dataset):
+    def __init__(self, mode='train', transform=None, target_transform=None):
+        self.mode = mode
+        self.img_dir = f'breakup_set/train' if mode == 'train' else 'breakup_set/test'
+        self.transform = transform
+        self.target_transform = target_transform
+        for root, dirs, files in os.walk(self.img_dir, topdown=False):
+            self.image_files = [f for f in files if f.endswith('.jpg')]
+
+    def __len__(self):
+        return len(self.image_files)
+
+    def __getitem__(self, idx):
+        name = random.randrange(1, 300) if self.mode == 'train' else random.randrange(301, 600)
+        img_name = f'02-000{name:03d}.jpg'
+        img_path = os.path.join(self.img_dir, img_name)
+        image = read_image(img_path)
+        gt_path = img_path.replace('.jpg', '.json')
+        with open(gt_path) as gt_file:
+            gt_list = json.load(gt_file)
+            gt = [[value for key, value in x.items()] for x in gt_list]
+        if self.transform:
+            image = self.transform(image)
+        return image, gt
